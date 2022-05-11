3419bd3a986083c6d32720e0a43eb56331e7423f
rafi-am7 <rafialmahmudc@gmail.com>
2022-05-11 07:12:41 +0600

    test1created

---
+++ test1.java
@@ -0,0 +1,24 @@
+
+class Test{
+   int number ;
+   String name ;
+   public void set_Name(String s){
+       name=s;
+        
+   }
+   public void Set( int n){
+       number=n;
+   }
+   public String get_Name(){
+       return name;
+   }
+    public int  get_Number(){
+       return number;
+   }
+   public Test (int a,String b){
+       number=a;
+       name=b;
+
+   }
+
+}
